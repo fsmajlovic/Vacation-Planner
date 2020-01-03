c5f892d3cd284e2472d701636a6083a590066a9d
Faruk Smajlovic <fsmajlovic2@etf.unsa.ba>
2020-01-03 15:51:43 +0100

    Admin Controller 1.Greetings message shows Username of the administrator that is logged in. 2.ChoiceBox is binded to SelectedMonth label (All months is selected by default). 3.Logout button returns user to the LoginScreen.

---
+++ src/Projekat/AdminController.java
@@ -0,0 +1,48 @@
+package Projekat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AdminController{
+    public ChoiceBox<String> choiceSelectMonth;
+    public Label GreetingsLabel;
+    public Label selectedMonthLabel;
+    public MonthsModel model = new MonthsModel();
+    public void LogoutAdminOnAction(ActionEvent actionEvent) throws IOException {
+        Parent AdminParent = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
+        Scene AdminScene = new Scene(AdminParent, 500, 300);
+        Stage window = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        window.setScene(AdminScene);
+        window.show();
+    }
+
+
+    public void initData() {
+        model.napuni();
+        choiceSelectMonth.setItems(model.getMonths());
+        selectedMonthLabel.textProperty().bind(model.getCurrentSimple());
+    }
+
+    public void initGreetingsMsg(String name){
+        GreetingsLabel.setText("Welcome " + name + "!");
+    }
+
+    public void changeMonth(ActionEvent actionEvent) {
+        if(choiceSelectMonth.getValue().equals("Select All")){
+            model.setCurrentMonth("All Months");
+        }
+        else {
+            model.setCurrentMonth(choiceSelectMonth.getValue());
+        }
+    }
+
+
+}
