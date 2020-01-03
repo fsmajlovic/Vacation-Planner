a29332005ac3bfa21aa677f9b7dcf76db8e9dbc4
Faruk Smajlovic <fsmajlovic2@etf.unsa.ba>
2020-01-03 15:39:10 +0100

    Login screen controller with Administrator access via CheckBox implemented. Standard Login sends you to the current month of the year. Username and Password still don't require to be correct. Sign up method yet to be implemented.

---
+++ src/Projekat/LoginController.java
@@ -0,0 +1,67 @@
+package Projekat;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+public class LoginController {
+
+    public CheckBox AdminCheckBox;
+    public boolean CheckBox = false;
+    public PasswordField password;
+    public TextField username;
+
+    public void LoginButtonAction(ActionEvent actionEvent) throws IOException {
+        if(CheckBox){
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("AdminScreen.fxml"));
+            Parent viewParent = loader.load();
+
+            Scene viewScene = new Scene(viewParent);
+
+            AdminController ctrl = loader.getController();
+            ctrl.initData();
+            ctrl.initGreetingsMsg(username.getText());
+
+            Stage window = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            window.setScene(viewScene);
+            window.show();
+
+        }
+        else{
+            String month = getMonthName();
+
+            Parent MonthParent = FXMLLoader.load(getClass().getResource("January" + ".fxml"));
+            Scene MonthScene = new Scene(MonthParent, 800, 500);
+            Stage window = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+            window.setScene(MonthScene);
+            window.show();
+        }
+
+    }
+
+    public void AdminCheckBoxOnAction(ActionEvent actionEvent) {
+        if(CheckBox == false)
+            CheckBox = true;
+        else if(CheckBox == true)
+            CheckBox = false;
+    }
+
+    public String getMonthName(){
+        String[] monthName = {"January", "February",
+                "March", "April", "May", "June", "July",
+                "August", "September", "October", "November",
+                "December"};
+        Calendar cal = Calendar.getInstance();
+        return monthName[cal.get(Calendar.MONTH)];
+    }
+}
